9b92ebad8925139700f2f7f7292ffacff7994487
Jordan Adkins <jordan@jordans-air-2.lan>
2021-01-02 20:55:14 -0700

    Employee REST Service

---
+++ Tut/src/test/java/com/PayRoll/Tut/TutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PayRoll.Tut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
