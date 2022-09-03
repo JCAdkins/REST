ae29ba61d2bffb59ecb8e24fd7c3007da5996bdf
JCAdkins <52946099+JCAdkins@users.noreply.github.com>
2022-09-03 15:42:10 -0700

    Delete Tut directory

--- Tut/src/main/java/com/PayRoll/Tut/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.PayRoll.Tut;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    EmployeeNotFoundException(Long id){
-        super("Could not find employee " + id);
-    }
-}
--- Tut/src/main/java/com/PayRoll/Tut/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.PayRoll.Tut;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface EmployeeRepository extends JpaRepository<Employee, Long>{
-}
